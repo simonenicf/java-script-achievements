fa2aabf16d968602b6822bc885ee7e970261c8fe
michelle frankfort <j.s.frankfort@gmail.com>
2020-12-03 10:23:28 +0100

    Made text to speech website

---
+++ B2L3TextToSpeech/script.js
@@ -0,0 +1,68 @@
+"use strict"   
+    let myText = document.getElementById('myText');
+    let myImage = document.getElementById('myImage');
+    let myButton = document.getElementById('myButton');
+    let myReset = document.getElementById('myReset');
+
+    myButton.addEventListener('click', function(){
+        main();
+    })
+
+    myReset.addEventListener('click', function(){
+        reset();
+    })
+
+    function reset() {
+        myImage.src = "images/reset.jpg"
+        myText.innerHTML = "...."
+    }
+
+    function main(){
+         let dezeTextWordtHet = makeThisSentence();
+         myText.innerHTML = dezeTextWordtHet;
+         sayItLoud(dezeTextWordtHet); // say something nice say it loud
+         selectThisImage(); // show a nice image
+    }
+    
+    
+    
+    function randomizer(range = 1){ 
+        // Math.random geeft een waarde  0 <= waarde < 1 (dus exclusief 1)
+        return Math.floor((Math.random() * range));
+    }
+    
+    
+    function makeThisSentence(){
+        let index = randomizer(arrayLength);
+        let index2 = randomizer(arrayLength);
+        let index3 = randomizer(arrayLength);
+        let woord1 = onderwerp[index];
+        let woord2 = werkwoord[index2];
+        let woord3 = restwoord[index3];
+        let outputString = woord1 + " " + woord2 + " " + woord3;
+        return outputString;
+    }
+    
+    function selectThisImage(){
+        let index = randomizer(arrayLength2);
+        myImage.src = plaatjes[index]; 
+    }
+    
+    function sayItLoud (textString) {
+      let message = new SpeechSynthesisUtterance(textString);
+      let voices = window.speechSynthesis.getVoices();
+      message.voice = voices[1];
+      message.pitch = 1; // range between 0 and 2
+      message.rate = 1; // range between 0.1 (lowest) and 10 (highest) 
+      window.speechSynthesis.speak(message);
+    }
+    
+    
+    
+    const onderwerp = ["Cheese", "Mario", "Zombies", "My Hero", "bees"];
+    const werkwoord = ["eats", "flys", "sings", "kills", "hug"];
+    const restwoord = ["pie", "basement", "death metal", "People", "cannibals"];
+        
+    let plaatjes = ["images/bees.jpg", "images/cheese.jpg", "images/mario.jpg", "images/zombie.jpg", "images/Death-metal.jpg", "images/cannibals.jpg"]
+    let arrayLength = onderwerp.length;
+    let arrayLength2 = plaatjes.length;
